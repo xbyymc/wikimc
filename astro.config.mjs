9e77c9683b1fa825a7f6a4f639c04d2870729cc1
xbyymc <56867863+xbyymc@users.noreply.github.com>
2026-04-29 11:57:45 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,16 +9,29 @@ export default defineConfig({
 		starlight({
 			title: '维基MC',
 			defaultLocale: 'zh-CN',
+			locales: {
+				'zh-CN': { label: '简体中文' },
+			},
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			editLink: {
         		baseUrl: 'https://github.com/xbyymc/wikimc/blob/main/',
       		},
 			lastUpdated: true,
 			sidebar: [
-				{
-					label: '生存路线',
-					autogenerate: { directory: 'survival' },
-				},
+  				{
+    				locales: {
+      					en: [
+        					{ autogenerate: { directory: 'en/survival' } },
+        					{ autogenerate: { directory: 'en/guide' } },
+      					],
+      					'zh-CN': [
+        					{ autogenerate: { directory: 'zh-CN/survival' } },
+        					{ autogenerate: { directory: 'zh-CN/guide' } },
+      					],
+    				},
+  				},
+  				{ label: 'SkinMC', link: 'https://skinmc.cn' },
+  				{ label: 'HubMC', link: 'https://hubmc.cn' },
 			],
 		}),
 	],
