c6ace81a7eb97b2bf22b013948de3b095343d962
xbyymc <56867863+xbyymc@users.noreply.github.com>
2026-04-29 10:55:12 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,6 @@ export default defineConfig({
 		starlight({
 			title: '维基MC',
 			defaultLocale: 'zh-CN',
-      		locales: {
-        		'zh-CN': { label: '简体中文' }
-      		},
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			editLink: {
         		baseUrl: 'https://github.com/xbyymc/wikimc/blob/main/',
